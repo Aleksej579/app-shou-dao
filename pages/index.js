cde6d030f61bd5681ebed1fae740c45d10eee509
Aleksej579 <aleksej.eremenko579@gmail.com>
2021-09-29 17:48:52 +0300

    try oficial documentation

--- pages/index.js
+++ pages/index.js
@@ -9,35 +9,27 @@ import ResourceListWithProducts from './components/ResourceList';
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 // const url_ = `https://05ce07551a14094626d7611a5aa64254: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@shopyfyliquid.myshopify.com/admin/api/2021-07/script_tags.json`;
-const url_ = `https://shopyfyliquid.myshopify.com/admin/api/2021-07/script_tags.json`;
-let options = {
-  method: 'post',
-  script_tag: {
-    event: 'onload',
-    src: 'https://unpkg.com/vue@next'
-  }
-}
-fetch(url_, options)
-  .then(response => response.json())
-  .then(result => response.text())
-
-
-// body: JSON.stringify({
-//   "script_tag": {
-//     "event": "onload"
-//     , "src": "https://21c3ed9616a8.ngrok.io/scripttag.js"
-//     , "display_scope": "all"
+// const url_ = `https://shopyfyliquid.myshopify.com/admin/api/2021-07/script_tags.json`;
+// let options = {
+//   method: 'post',
+//   script_tag: {
+//     event: 'onload',
+//     src: 'https://unpkg.com/vue@next'
 //   }
-// })
+// }
+// fetch(url_, options)
+//   .then(response => response.json())
+//   .then(result => response.text())
 
-// import Shopify, { DataType } from '@shopify/shopify-api';
 
-// const client = new Shopify.Clients.Rest('shopyfyliquid.myshopify.com', accessToken);
-// const data = client.post({
-//   path: 'script_tags',
-//   body: { "script_tag": { "event": "onload", "src": "https:\/\/djavaskripped.org\/fancy.js" } },
-//   type: DataType.JSON,
-// });
+import Shopify, { DataType } from '@shopify/shopify-api';
+
+const client = new Shopify.Clients.Rest('shopyfyliquid.myshopify.com', accessToken);
+const data = client.post({
+  path: 'script_tags',
+  body: { "script_tag": { "event": "onload", "src": "https:\/\/djavaskripped.org\/fancy.js" } },
+  type: DataType.JSON,
+});
 
 
 
